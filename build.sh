3e81047b44337f4d761dcf25f2e2b9ed6388d50b
Peter DeMartini <thepeterdemartini@gmail.com>
2015-10-01 09:34:56 -0700

    v1.0.1 do the build magic

--- build.sh
+++ build.sh
@@ -2,10 +2,50 @@
 
 echo "** Starting..."
 
-rm -rf dpl_s3
-mkdir -p dpl_s3
-rm -rf build
-mkdir build
+START_DIR=`pwd`
 
-echo "** Building..."
-./node_modules/.bin/electron-packager ./build/electron Gateblu --platform=all --arch=all --version=0.33.2
+if [ -z "$PLATFORMS" ]; then
+  PLATFORMS="darwin-x64 linux-x64 linux-ia32 win32-ia32 win32-x64"
+fi
+BUILD_DIR=$START_DIR/build
+CACHE_DIR=$BUILD_DIR/cache
+ELECTRON_VERSION=0.33.3
+
+mkdir -p build/cache/electron
+
+for ELECTRON_PLATFORM in $PLATFORMS; do
+  cd $CACHE_DIR/electron
+
+  echo "** Starting build for [$ELECTRON_PLATFORM]..."
+
+  JUST_ELECTRON_PLATFORM=`echo $ELECTRON_PLATFORM | sed "s/-x64//" | sed "s/-ia32//"`
+  JUST_ELECTRON_ARCH=`echo $ELECTRON_PLATFORM | sed "s/darwin-//" | sed "s/linux-//" | sed "s/win32-//"`
+
+  APP_ICON=$START_DIR/assets/gateblu-icon.icns
+  if [ $JUST_ELECTRON_PLATFORM == "win32" ]; then
+    APP_ICON=$START_DIR/assets/gateblu-icon.ico
+  fi
+
+  echo "** Packaging for [$ELECTRON_PLATFORM]..."
+
+  $START_DIR/node_modules/.bin/electron-packager $START_DIR/app Gateblu \
+      --platform=$JUST_ELECTRON_PLATFORM \
+      --arch=$JUST_ELECTRON_ARCH \
+      --version=$ELECTRON_VERSION \
+      --icon=$APP_ICON \
+      --out=$CACHE_DIR \
+      --cache=$CACHE_DIR/electron \
+      --overwrite \
+      --version-string.CompanyName=Octoblu \
+      --version-string.ProductName=Gateblu
+
+  PACKAGE_FOLDER=$CACHE_DIR/gateblu-v$ELECTRON_VERSION-$ELECTRON_PLATFORM
+
+  echo "** Renaming App [$ELECTRON_PLATFORM]..."
+  mv $CACHE_DIR/Gateblu-$ELECTRON_PLATFORM $PACKAGE_FOLDER
+
+  echo "** Zipping App [$ELECTRON_PLATFORM]..."
+  cd $PACKAGE_FOLDER
+  zip -9rq $PACKAGE_FOLDER.zip *
+
+done
